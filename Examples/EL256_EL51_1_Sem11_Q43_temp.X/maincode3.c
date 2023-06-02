c9a657cd2bcafe9243a0d119db9d21f235442117
Kalun Lau <klnlau@gmail.com>
2023-06-02 17:52:51 -0500

    Add files via upload

--- Examples/EL256_EL51_1_Sem11_Q43_temp.X/maincode3.c
+++ Examples/EL256_EL51_1_Sem11_Q43_temp.X/maincode3.c
@@ -5,6 +5,7 @@
 
 unsigned char diezmillar, millar, centena, decena, unidad;
 unsigned int resultado_ADC;
+unsigned char nivel = 0;
 
 void configuro(void){
     //configuracion de reloj
@@ -19,17 +20,29 @@ void configuro(void){
     WPUBbits.WPUB0 = 0;
     TRISFbits.TRISF0 = 0;
     ANSELFbits.ANSELF0 = 0;
-    TRISFbits.TRISF7 = 1;
-    ANSELFbits.ANSELF7 = 0;
+    TRISFbits.TRISF1 = 1;
+    ANSELFbits.ANSELF1 = 0;
     TRISD = 0x00;
     ANSELD = 0x00;
     //configuracion del ADC
-    ADCON0bits.ADFM = 1;    //justificacion a la izquierda
+    ADCON0bits.ADFM = 1;    //justificacion a la derecha
     ADCON0bits.CS = 1;      //fuente de reloj ADCRC
     ADPCH = 0x00;           //canal analógico RA0
     ADCON0bits.ADON = 1;      //ADC On
     ADREF = 0x03;           //Vref+ conectado a FVR_buf1
     FVRCON = 0x81;          //modulo FVR entregando 1.024V a FVR_buf1
+    //configuracion del CCP1 para que entregue PWM al contraste del LCD
+    T2PR = 62;               //frecuencia de PWM a 1KHz
+    RC2PPS = 0x15;          //PPS asignación de RC2 hacia CCP1
+    CCP1CON = 0x9C;         //CCP1 ON, justificacion izquierda, PWM mode
+    CCPR1H = 19;
+    CCPR1L = 0;             //duty cycle a 50%
+    T2CLKCON = 0x01;        //Timer2 Fosc/4
+    T2CON = 0xC0;           //Timer2 ON psc 1:16
+    //configuracion de las interrupciones
+    INTCON0bits.GIE = 1;
+    PIE1bits.INT0IE = 1;
+    INTCON0bits.INT0EDG = 0;
     //configuracion inicial del LCD
     LCD_INIT();
 }
@@ -42,10 +55,10 @@ void convierte(unsigned int numero){
     unidad = numero % 10;
 }
 
-void tomamuestra_ADC(void){
+unsigned int tomamuestra_ADC(void){
     ADCON0bits.GO = 1;
     while(ADCON0bits.GO == 1);
-    resultado_ADC = (ADRESH << 8) + ADRESL;
+    return (ADRESH << 8) + ADRESL;
 }
 
 void main(void) {
@@ -62,5 +75,39 @@ void main(void) {
         ENVIA_CHAR(unidad+0x30);
         ENVIA_CHAR(0xDF);
         ENVIA_CHAR('C');
+    }
+}
+
+void __interrupt(irq(IRQ_INT0)) INT0_ISR(void){
+    PIR1bits.INT0IF = 0;
+    switch(nivel){
+        case 0:
+            CCPR1H = 5;
+            nivel = 1;
+            break;
+        case 1:
+            CCPR1H = 15;
+            nivel = 2;
+            break;
+        case 2:
+            CCPR1H = 25;
+            nivel = 3;
+            break;
+        case 3:
+            CCPR1H = 35;
+            nivel = 4;
+            break;
+        case 4:
+            CCPR1H = 45;
+            nivel = 5;
+            break;
+        case 5:
+            CCPR1H = 55;
+            nivel = 6;
+            break;
+        case 6:
+            CCPR1H = 62;
+            nivel = 0;
+            break;
     }
 }
