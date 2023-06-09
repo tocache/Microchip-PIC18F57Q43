858e38ef19bfa51d9c5a540d5c3dc287507663c7
Kalun Lau <klnlau@gmail.com>
2023-06-09 13:02:54 -0500

    Add files via upload

---
+++ Examples/EL256_EL53_1_Sem12_Q43_UARTtest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EL256_EL53_1_Sem12_Q43_UARTtest.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EL256_EL53_1_Sem12_Q43_UARTtest.X.production.hex
