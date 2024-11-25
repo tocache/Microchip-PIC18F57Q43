046cfc597ffdd03fbe4b8c661a1079590cbb650e
Kalun Lau <klnlau@gmail.com>
2024-11-25 11:23:52 -0500

    Update LIB_WS2812.h

--- Libs/LIB_WS2812.h
+++ Libs/LIB_WS2812.h
@@ -1,25 +1,30 @@
 /*
-LibrerÌa WS2812 desarrollada por Kalun Lau para ser usado con
+Librer√≠a WS2812 desarrollada por Kalun Lau para ser usado con
 el microcontrolador PIC18F57Q43 y PIC18F57Q84.
-La formaciÛn de la trama de datos para el WS2812 se basÛ en
-una implementacion usando SPI, Timer2, CCP en PWM y CLB
+La formaci√≥n de la trama de datos para el WS2812 se bas√≥ en
+una implementacion usando SPI, Timer2, CCP en PWM y CLB que hizo
+Microchip en el siguiente ejemplo para el PIC16F13145 (https://github.com/microchip-pic-avr-examples/pic16f13145-spi-ws2812-mplab-mcc).
+Video de referencia (emplea el MCC): https://www.youtube.com/watch?v=hHLMnJs87VE
+Documento de referencia para el protocolo del WS2812: http://ww1.microchip.com/downloads/en/AppNotes/00001606A.pdf
+
+Para esta implementaci√≥n no se us√≥ el MCC ni el Melody, esta hecho con pura configuraci√≥n de registros y c√≥digo en C.
+
 Curso de Microcontroladores
 Universidad Peruana de Ciencias Aplicadas
 Lima - Peru
 
-
 Changelog:
-25/11/2024 CreaciÛn de la librerÌa
+25/11/2024 Creaci√≥n de la librer√≠a
  */
 
 #ifndef LIB_WS2812_H
 #define	LIB_WS2812_H
 
-/*FunciÛn de inicializaciÛn de
- comunicaciÛn para WS2812*/
+/*Funci√≥n de inicializaci√≥n de
+ comunicaci√≥n para WS2812*/
 void WS2812_INIT(void);
 
-/*FunciÛn para enviar datos de
+/*Funci√≥n para enviar datos de
 los colores del WS2812*/
 void WS2812_DATA_SEND(unsigned char green, unsigned char red, unsigned char blue);
 
