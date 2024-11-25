7175a522c2308746a2f7492e828b0af98fa0c1f2
Kalun Lau <klnlau@gmail.com>
2024-11-25 11:15:38 -0500

    Update LIB_DHT.h

--- Libs/LIB_DHT.h
+++ Libs/LIB_DHT.h
@@ -5,6 +5,14 @@
  * Created on 10 de junio de 2024, 03:49 PM
  */
 
+/* Librer√≠a desarrollada por Kalun Lau en base a
+un ejemplo de interface que no me acuerdo en donde lo v√≠
+
+Changelog:
+25/11/2024 Le coloqu√© una opci√≥n rudimentaria de timeout
+para que no se quede colgado el programa en los whiles
+*/
+
 #ifndef LIB_DHT_H
 #define	LIB_DHT_H
 
@@ -14,22 +22,22 @@
 #define DHT11 0
 #define DHT22 1
 
-/*FunciÛn para la etapa de inicio de comunicacion con el DHTxx*/
+/*Funci√≥n para la etapa de inicio de comunicacion con el DHTxx*/
 void DHT_Start(unsigned char modelo);
 
-/*FunciÛn para ver si el DHTxx esta disponible DHTxx*/
+/*Funci√≥n para ver si el DHTxx esta disponible DHTxx*/
 void DHT_Check(unsigned char modelo);
 
-/*FunciÛn para hacer lectura de un dato de 8 bits del DHTxx*/
+/*Funci√≥n para hacer lectura de un dato de 8 bits del DHTxx*/
 unsigned char DHT_Read(unsigned char modelo);
 
-/*FunciÛn para obtener el valor de temperatura del DHTxx
- Tener en cuenta que para el DHT22 se deber· de dividir entre 10
+/*Funci√≥n para obtener el valor de temperatura del DHTxx
+ Tener en cuenta que para el DHT22 se deber√° de dividir entre 10
  el valor obtenido*/
 unsigned int DHT_GetTemp(unsigned char modelo);
 
-/*FunciÛn para obtener el valor de humedad relativa del DHTxx
- Tener en cuenta que para el DHT22 se deber· de dividir entre 10
+/*Funci√≥n para obtener el valor de humedad relativa del DHTxx
+ Tener en cuenta que para el DHT22 se deber√° de dividir entre 10
  el valor obtenido*/
 unsigned int DHT_GetHumid(unsigned char modelo);
 
