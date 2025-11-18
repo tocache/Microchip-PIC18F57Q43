d084a2ee05e8fe736acdf9b517a09fe133a9829f
Kalun Lau <klnlau@gmail.com>
2025-11-17 19:59:09 -0500

    Update LIB_DHT.h

--- Libs/LIB_DHT.h
+++ Libs/LIB_DHT.h
@@ -9,6 +9,17 @@
 un ejemplo de interface que no me acuerdo en donde lo ví
 
 Changelog:
+17/11/2025 He creado una nueva función en base a struct
+para que retorne las dos medidas (temperatura y humedad)
+en base a una sola acción de lectura del sensor.
+
+La función ejemplo sería asi:
+struct DHT_Values actuales = DHT_GetBoth(DHT11);
+Para sacar el valor de temperatura:
+actuales.DHT_Temp
+Para sacar el valor de humedad:
+actuales.DHT_Humid
+
 25/11/2024 Le coloqué una opción rudimentaria de timeout
 para que no se quede colgado el programa en los whiles
 */
@@ -41,5 +52,17 @@ unsigned int DHT_GetTemp(unsigned char modelo);
  el valor obtenido*/
 unsigned int DHT_GetHumid(unsigned char modelo);
 
+struct DHT_Values{
+    unsigned int DHT_Temp;
+    unsigned int DHT_Humid;
+};
+
+/*Función para obtener los valores de humedad relativa y temperatura
+ del DHTxx en una sola acción de lectura al sensor.
+ Tener en cuenta que para el DHT22 se deberá de dividir entre 10
+ el valor obtenido*/
+struct DHT_Values DHT_GetBoth(unsigned char modelo);
+
 #endif	/* LIB_DHT_H */
+
 
